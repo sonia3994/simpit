56ac808fdec3ac4d7de3951be7bad79cba649906
xuchenghai@gmail.com <xuchenghai@gmail.com@3cf4116c-e284-11de-9129-6395494df245>
2010-06-24 12:21:29 +0000

    GPCaptureFieldManager change QWT field.

--- branches/simpit-1.3/src/GPCaptureFieldManager.cc
+++ branches/simpit-1.3/src/GPCaptureFieldManager.cc
@@ -203,7 +203,9 @@ void GPCaptureField::GetFieldValueQWTNegativeSqr(const G4double Point[3], G4doub
   	if(relativeZ>0&&relativeZ<=capL&&localR2<=sqrCapR)
 	{
 		feiMi=1/(1+qwtAlpha*relativeZ*relativeZ/cm/cm);
+  		//Bfield[0]=0;
   		Bfield[0]=Point[0]*B0*qwtAlpha*relativeZ*feiMi*feiMi/cm/cm;
+  		//Bfield[1]=0;
   		Bfield[1]=Point[1]*Bfield[0]/Point[0];
 		Bfield[2]=B0*feiMi;
 	}
