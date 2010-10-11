69d574ec7228b4f0b939c7334f862d16030382db
xuchenghai <xuchenghai@3cf4116c-e284-11de-9129-6395494df245>
2010-10-11 07:37:53 +0000

    Copy branches/simpit-1.8 to trunk/simpit-1.8

---
+++ simpit-1.8/src/GPSteppingAction.cc
@@ -0,0 +1,118 @@
+// $Id: GPSteppingAction.cc,v 1.15 2006/06/29 17:49:13 gunter Exp $
+// GEANT4 tag $Name: geant4-09-02 $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "GPSteppingAction.hh"
+#include "GPSteppingMessenger.hh"
+
+#include "GPDetectorConstruction.hh"
+#include "GPEventAction.hh"
+#include "GPRunAction.hh"
+
+#include "G4ThreeVector.hh"
+#include "G4RunManager.hh"
+#include "G4Step.hh"
+
+//#include <fstream>
+#include <sstream>
+using namespace std;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+GPSteppingAction::GPSteppingAction(GPDetectorConstruction* det,
+                                         GPEventAction* evt)
+:detector(det), eventAction(evt)					 
+{ 
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Enter GPSteppingAction::GPSteppingAction(GPDetectorConstruction*)"<<G4endl;
+#endif
+  sParticle="e+";
+  steppingMessenger = new GPSteppingMessenger(this);
+  verbose=0;
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Exit GPSteppingAction::GPSteppingAction(GPDetectorConstruction*)"<<G4endl;
+#endif
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+GPSteppingAction::~GPSteppingAction()
+{ 
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Enter GPSteppingAction::~GPSteppingAction()"<<G4endl;
+#endif
+  delete 	steppingMessenger;
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Exit GPSteppingAction::~GPSteppingAction()"<<G4endl;
+#endif
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void GPSteppingAction::Init()
+{
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Enter GPSteppingAction::Init()"<<G4endl;
+#endif
+  targetPhys = detector->GetTargetPhysical(); 
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Exit GPSteppingAction::Init()"<<G4endl;
+#endif
+}
+
+void GPSteppingAction::UserSteppingAction(const G4Step* aStep)
+{
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Enter GPSteppingAction::UserSteppingAction(const G4Step*)"<<G4endl;
+#endif
+  static G4VPhysicalVolume* 	prevPhys;
+  static G4Track*		currentTrack;
+  static G4TrackStatus		currentTrackStatus;
+  static G4double		stepE;
+  static G4double		stepL;
+  static G4String 		particleName;
+  static G4int   		stopFlag;
+
+  prevPhys= aStep->GetPreStepPoint()->GetPhysicalVolume();
+  currentTrack=aStep->GetTrack();
+  currentTrackStatus=fStopAndKill;
+  stepE=aStep->GetTotalEnergyDeposit();
+
+  if(stepE<0) {return;}
+  
+  stepL = aStep->GetStepLength();
+  if(stepL<=0)
+  {
+    stopFlag++;
+    if(stopFlag>=8)
+    {
+      currentTrack->SetTrackStatus(currentTrackStatus);
+      if (verbose>=1)
+      {
+        G4cout<<"Kill a particle because it does not move more then 8 steps: "<<particleName<<G4endl;
+        return;
+      }
+    }
+  }
+  else 
+  { stopFlag=0; }
+
+  if (prevPhys==targetPhys) 
+  {
+    eventAction->AddTargetED(stepE);
+    eventAction->AddTargetStep(stepL);
+  }
+#ifdef GP_DEBUG
+  G4cout<<"GP_DEBUG: Exit GPSteppingAction:::UserSteppingAction(const G4Step*)"<<G4endl;
+#endif
+}
+
+void GPSteppingAction::SetSelectedParticle(G4String tmpParticle)
+{
+  sParticle=tmpParticle;
+  G4cout<<"The filter particle is:"<<sParticle<<G4endl;
+}
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
