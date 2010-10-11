95a9e6a98fe09303e5e3c63d33337fce89683667
xuchenghai <xuchenghai@3cf4116c-e284-11de-9129-6395494df245>
2010-10-11 07:37:53 +0000

    Copy branches/simpit-1.8 to trunk/simpit-1.8

---
+++ trunk/simpit-1.8/include/GPSteppingAction.hh
@@ -0,0 +1,46 @@
+// $Id: GPSteppingAction.hh,v 1.10 2007/07/02 13:22:08 vnivanch Exp $
+// GEANT4 tag $Name: geant4-09-02 $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#ifndef GPSteppingAction_h
+#define GPSteppingAction_h 1
+
+#include "G4UserSteppingAction.hh"
+#include "G4ThreeVector.hh"
+#include "globals.hh"
+
+class GPSteppingMessenger;
+class GPDetectorConstruction;
+class GPEventAction;
+class G4VPhysicalVolume;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class GPSteppingAction : public G4UserSteppingAction
+{
+public:
+  GPSteppingAction(GPDetectorConstruction*, GPEventAction*);
+  virtual ~GPSteppingAction();
+
+  void UserSteppingAction(const G4Step*);
+  void SetSelectedParticle(G4String);
+  void Init();
+    
+private:
+  GPDetectorConstruction* detector;
+  GPEventAction*          eventAction;
+  GPSteppingMessenger*	  steppingMessenger;
+  const G4VPhysicalVolume* targetPhys;
+
+  G4String 		sParticle;
+  G4int   verbose;
+  
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
