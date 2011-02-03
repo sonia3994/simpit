192fcd06aa069b5f8fd4cefde0e7a6e093c85707
xuchenghai <xuchenghai@3cf4116c-e284-11de-9129-6395494df245>
2011-02-03 09:02:05 +0000

    svn copy branches/simpit-1.9/ trunk/simit-1.9/; svn copy branches/simpit-1.9/ branches/simit-2.0

---
+++ branches/simit-2.0/include/GPEventAction.hh
@@ -0,0 +1,58 @@
+// $Id: GPEventAction.hh,v 1.12 2007/07/02 13:22:08 vnivanch Exp $
+// GEANT4 tag $Name: geant4-09-02 $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#ifndef GPEventAction_h
+#define GPEventAction_h 1
+#include "GPTargetHit.hh"
+#include "GPParticleHit.hh"
+
+#include "globals.hh"
+#include "G4ThreeVector.hh"
+#include "G4UserEventAction.hh"
+#include <fstream>
+using namespace std;
+
+class GPEventActionMessenger;
+//class GPParticleHitsCollection;
+//class GPTargetHitsCollection;
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class GPEventAction : public G4UserEventAction// :public fstream
+{
+public:
+  GPEventAction();
+  virtual ~GPEventAction();
+
+  void  BeginOfEventAction(const G4Event*);
+  void  EndOfEventAction(const G4Event*);
+  inline  G4int GetEventID() const {return iEventID;};
+  inline void  SetPrintModulo(G4int    val)  {iPrintModel = val;};
+  inline void AddTargetED(G4double de) {dEnergyTar+=de;};
+  inline void AddTargetStep(G4double TargetStep) {dTrackL+=TargetStep;}; 
+  void  AddPositron(G4ThreeVector,G4ThreeVector,G4double);                  
+  void  SetTrajectoryFlag(G4int i){iTrajectoryFlag=i;};                  
+  void ProcessParticleHits(GPParticleHitsCollection*,G4String);
+  void ProcessEdd(GPTargetHitsCollection* );
+    
+private:
+   G4double  dEnergyTar;
+   G4double  dTrackL;
+   G4int     iPrintModel;
+   G4int     iNPositronPerEvt;
+   G4int     iEventID;
+    //-1, record all trajectories, 0, doesn't record, other positive n, record n events' trajectory.
+   G4int     iTrajectoryFlag;
+                             
+   GPEventActionMessenger*  eventMessenger;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
+    
---
+++ branches/simit-2.0/include/GPTargetROGeometry.hh
@@ -0,0 +1,51 @@
+#ifndef GPTargetROGeometry_h
+#define GPTargetROGeometry_h 1
+
+#include "G4VReadOutGeometry.hh"
+#include <vector> 
+
+class G4Material;
+
+class GPTargetROGeometry : public G4VReadOutGeometry
+{
+public:
+  GPTargetROGeometry();
+  GPTargetROGeometry(G4String name, G4double x, G4double y, G4double z,std::vector<G4int> n);
+  ~GPTargetROGeometry();
+
+private:
+  G4VPhysicalVolume* Build();
+  G4Material* dummyMat;
+  void	Init();
+
+
+     G4int iTargetBox_RepX;
+     G4int iTargetBox_RepY;
+     G4int iTargetBox_RepZ;
+
+     G4double dExpHall_x;
+     G4double dExpHall_y;
+     G4double dExpHall_z;
+
+     G4double dTargetBox_x;
+     G4double dTargetBox_y;
+     G4double dTargetBox_z;
+
+     G4double dTargetBox_dx;
+     G4double dTargetBox_dy;
+     G4double dTargetBox_dz;
+
+     G4double dTargetBoxXCell_x;
+     G4double dTargetBoxXCell_y;
+     G4double dTargetBoxXCell_z;
+
+     G4double dTargetBoxYCell_x;
+     G4double dTargetBoxYCell_y;
+     G4double dTargetBoxYCell_z;
+
+     G4double dTargetBoxZCell_x;
+     G4double dTargetBoxZCell_y;
+     G4double dTargetBoxZCell_z;
+};
+
+#endif
